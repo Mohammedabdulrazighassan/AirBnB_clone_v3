5048fd5819decbd709764261b66abdb6451c7d6c
Mohammedabdulrazighassan <mahammd_2010@hotmail.com>
2024-08-06 23:20:38 +0000

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,29 @@ Create Flask app; app_views
 '''
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status')
 def api_status():
 """
-
+Returns a JSON response for RESTful API health.
 """
 response = {'status': "ok"}
 return jsonify(response)
+
+@app_views.route('/stats')
+def get_stats():
+"""
+
+
+"""
+stats = {
+	'amenities': storage.count('Amenity'),
+	'cities': storage.count('City'),
+	'places': storage.count('Place'),
+	'reviews' : storage.count('Review'),
+	'states' : storage.count('State'),
+	'usres' : storage.count('User'),
+}
+
+return jsonify(stats)
