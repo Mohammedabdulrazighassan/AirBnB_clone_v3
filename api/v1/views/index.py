fdd83228538bb978ac647060c2b55517a1a6655b
Mohammedabdulrazighassan <mahammd_2010@hotmail.com>
2024-08-06 01:14:03 +0000

    M

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,15 @@
 #!/usr/bin/python3
-"""Define routes for blueprint
+'''
+Create Flask app; app_views
+'''
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def api_status():
 """
 
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
-
-
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """Return status of application
-    """
-    return jsonify({'status': 'OK'})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def stats():
-    """Retrieve count of objects in storage
-    """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-
-    classes = {"amenities": Amenity, "cities": City,
-               "places": Place, "reviews": Review,
-               "states": State, "users": User}
-    json_dict = {}
-
-    for name, cls in classes.items():
-        json_dict.update({name: storage.count(cls)})
-
-    return jsonify(json_dict)
+"""
+response = {'status': "ok"}
+return jsonify(response)
